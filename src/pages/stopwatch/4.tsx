79e251a08434703574d45f33b3468085d05b7307
Tung Vu <me@tungv.com>
2021-11-12 05:49:50 +0700

    feat: setup mdx
    Signed-off-by: Tung Vu <me@tungv.com>

--- src/pages/stopwatch/4.tsx
+++ src/pages/stopwatch/4.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+
+import { readFileSync } from "node:fs";
+import path from "node:path";
 
 import StopWatchLesson from "../../compoment/Apps/StopWatchApp";
 import StopWatchApp from "../../lessons/stopwatch/StopWatchAppWithAnimatedDuration";
 
-export default function StopWatchAppStep1() {
+export default function StopWatchAppStep1({ source }) {
   return (
     <StopWatchLesson srcFilePath="lessons/stopwatch/StopWatchAppWithAnimatedDuration.tsx">
       <StopWatchApp />
+
+      <MDXRemote {...source} />
     </StopWatchLesson>
   );
+}
+
+export async function getStaticProps() {
+  const source = String(
+    readFileSync(
+      path.join(process.cwd(), "src/lessons/stopwatch/animated-duration.mdx"),
+    ),
+  );
+  const mdxSource = await serialize(source);
+  return { props: { source: mdxSource } };
 }
