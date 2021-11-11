979b5b8c18bb1f19a550d1e034b57c797beda2cc
Tung Vu <me@tungv.com>
2021-11-12 05:19:21 +0700

    [stopwatch] add animated duration scaffold
    Signed-off-by: Tung Vu <me@tungv.com>

---
+++ src/lessons/stopwatch/StopWatchAppWithAnimatedDuration.tsx
@@ -0,0 +1,108 @@
+import { PropsWithChildren, useState } from "react";
+
+export default function StopWatchApp() {
+  const [hasStarted, setHasStarted] = useState(false);
+  const [isPausing, setIsPausing] = useState(false);
+
+  function start() {
+    setHasStarted(true);
+    setIsPausing(false);
+  }
+
+  function stop() {
+    setIsPausing(true);
+  }
+
+  function resume() {
+    setIsPausing(false);
+  }
+
+  function reset() {
+    setHasStarted(false);
+    setIsPausing(false);
+  }
+
+  return (
+    <div>
+      {!hasStarted && <StopWatchInitial requestStart={start} />}
+      {hasStarted && !isPausing && <StopWatchRunning requestStop={stop} />}
+      {hasStarted && isPausing && (
+        <StopWatchingStopped requestReset={reset} requestResume={resume} />
+      )}
+    </div>
+  );
+}
+
+function StopWatchLayout(props: PropsWithChildren<{}>) {
+  return (
+    <section>
+      {props.children}
+
+      <style jsx>{`
+        section {
+          margin: auto;
+          width: 320px;
+          padding: 16px;
+          border: 2px solid hsl(0 50% 50%);
+          border-radius: 8px;
+        }
+      `}</style>
+    </section>
+  );
+}
+
+function Duration({ durationMs }: { durationMs: number }) {
+  const hh = String(Math.floor(durationMs / 1000 / 60 / 60)).padStart(2, "0");
+  const mm = String(Math.floor(durationMs / 1000 / 60) % 60).padStart(2, "0");
+  const ss = String(Math.floor(durationMs / 1000) % 60).padStart(2, "0");
+  const ms = String(durationMs % 1000).padStart(3, "0");
+
+  return (
+    <div>
+      <span>{hh}</span>:<span>{mm}</span>:<span>{ss}</span>.<span>{ms}</span>
+    </div>
+  );
+}
+
+function StopWatchInitial({ requestStart }: { requestStart: () => void }) {
+  return (
+    <StopWatchLayout>
+      <h4>Initial</h4>
+      <Duration durationMs={0} />
+      <footer className="flex flex-row gap-1">
+        <button onClick={requestStart}>Start</button>
+      </footer>
+    </StopWatchLayout>
+  );
+}
+
+function StopWatchRunning({ requestStop }: { requestStop: () => void }) {
+  return (
+    <StopWatchLayout>
+      <h4>running…</h4>
+      <Duration durationMs={562739} />
+      <footer className="flex flex-row gap-1">
+        <button onClick={requestStop}>stop</button>
+      </footer>
+    </StopWatchLayout>
+  );
+}
+
+function StopWatchingStopped({
+  requestResume,
+  requestReset,
+}: {
+  requestResume: () => void;
+  requestReset: () => void;
+}) {
+  return (
+    <StopWatchLayout>
+      <h4>stopped</h4>
+      <Duration durationMs={562739} />
+      <footer className="flex flex-row gap-1">
+        <button onClick={requestResume}>resume</button>
+        <button onClick={requestReset}>reset</button>
+      </footer>
+    </StopWatchLayout>
+  );
+}
--- src/pages/stopwatch/4.tsx
+++ src/pages/stopwatch/4.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import StopWatchLesson from "../../compoment/Apps/StopWatchApp";
-import StopWatchApp from "../../lessons/stopwatch/stage-1";
+import StopWatchApp from "../../lessons/stopwatch/StopWatchAppWithAnimatedDuration";
 
 export default function StopWatchAppStep1() {
   return (
-    <StopWatchLesson srcFilePath="lessons/stopwatch/stage-1.tsx">
+    <StopWatchLesson srcFilePath="lessons/stopwatch/StopWatchAppWithAnimatedDuration.tsx">
       <StopWatchApp />
     </StopWatchLesson>
   );
